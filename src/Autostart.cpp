7c5d29c00d77b783dc98d7cca24f273d0f75d303
Dominik Cebula <dominikcebula@gmail.com>
2025-07-05 21:03:01 +0200

    Code cleanups

--- src/Autostart.cpp
+++ src/Autostart.cpp
@@ -1,4 +1,6 @@
 #include "Autostart.h"
+
+#include <string>
 #include <Windows.h>
 
 static const wchar_t* runKey = L"Software\\Microsoft\\Windows\\CurrentVersion\\Run";
@@ -11,9 +13,10 @@ bool IsAutostartEnabled()
     bool enabled = false;
     if (RegOpenKeyExW(HKEY_CURRENT_USER, runKey, 0, KEY_READ, &hKey) == ERROR_SUCCESS)
     {
-        wchar_t exePath[MAX_PATH];
-        DWORD size = sizeof(exePath);
-        const LONG result = RegQueryValueExW(hKey, appName, nullptr, nullptr, reinterpret_cast<LPBYTE>(exePath), &size);
+        std::wstring exePath(MAX_PATH, L'\0');
+        DWORD size = static_cast<DWORD>(exePath.size() * sizeof(wchar_t));
+        const LONG result = RegQueryValueExW(hKey, appName, nullptr, nullptr, reinterpret_cast<LPBYTE>(&exePath[0]),
+                                             &size);
         enabled = result == ERROR_SUCCESS;
         RegCloseKey(hKey);
     }
@@ -22,15 +25,16 @@ bool IsAutostartEnabled()
 
 void SetAutostart(bool enable)
 {
-    wchar_t exePath[MAX_PATH] = {};
-    GetModuleFileNameW(nullptr, exePath, MAX_PATH);
+    std::wstring exePath(MAX_PATH, L'\0');
+    GetModuleFileNameW(nullptr, &exePath[0], static_cast<DWORD>(exePath.size()));
+    exePath.resize(wcsnlen_s(exePath.c_str(), exePath.size()));
     HKEY hKey = nullptr;
     if (RegOpenKeyExW(HKEY_CURRENT_USER, runKey, 0, KEY_WRITE, &hKey) == ERROR_SUCCESS)
     {
         if (enable)
         {
-            RegSetValueExW(hKey, appName, 0, REG_SZ, reinterpret_cast<const BYTE*>(exePath),
-                           static_cast<DWORD>((wcsnlen_s(exePath, MAX_PATH) + 1) * sizeof(wchar_t)));
+            RegSetValueExW(hKey, appName, 0, REG_SZ, reinterpret_cast<const BYTE*>(exePath.c_str()),
+                           static_cast<DWORD>((exePath.size() + 1) * sizeof(wchar_t)));
         }
         else
         {
