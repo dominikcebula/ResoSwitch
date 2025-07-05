34113f8e6cd07c487dea1c97d7e9b251d25d8303
Dominik Cebula <dominikcebula@gmail.com>
2025-07-05 15:11:35 +0200

    Code cleanups

--- src/Hotkeys.cpp
+++ src/Hotkeys.cpp
@@ -20,19 +20,30 @@ void UnregisterResolutionHotkeys(HWND hwnd, size_t count)
     }
 }
 
-bool ParseShortcut(const std::wstring& shortcut, HotkeyInfo& out)
+static std::wstring getUpperCasedShortcut(const std::wstring& shortcut)
 {
-    out.modifiers = 0;
-    out.vk = 0;
-    std::wstring s = shortcut;
-    for (auto& c : s) c = towupper(c);
-    if (s.find(L"CTRL") != std::wstring::npos) out.modifiers |= MOD_CONTROL;
-    if (s.find(L"SHIFT") != std::wstring::npos) out.modifiers |= MOD_SHIFT;
-    if (s.find(L"ALT") != std::wstring::npos) out.modifiers |= MOD_ALT;
-    if (s.find(L"WIN") != std::wstring::npos) out.modifiers |= MOD_WIN;
+    std::wstring upperCasedShortcut = shortcut;
+    for (auto& c : upperCasedShortcut)
+        c = towupper(c);
+    return upperCasedShortcut;
+}
 
-    size_t pos = s.rfind(L'+');
-    std::wstring key = pos != std::wstring::npos ? s.substr(pos + 1) : s;
+static void ParseModifiers(std::wstring shortcut, HotkeyInfo& out)
+{
+    if (shortcut.find(L"CTRL") != std::wstring::npos)
+        out.modifiers |= MOD_CONTROL;
+    if (shortcut.find(L"SHIFT") != std::wstring::npos)
+        out.modifiers |= MOD_SHIFT;
+    if (shortcut.find(L"ALT") != std::wstring::npos)
+        out.modifiers |= MOD_ALT;
+    if (shortcut.find(L"WIN") != std::wstring::npos)
+        out.modifiers |= MOD_WIN;
+}
+
+static bool ParseLetterAndNumbers(HotkeyInfo& out, std::wstring shortcut)
+{
+    size_t pos = shortcut.rfind(L'+');
+    std::wstring key = pos != std::wstring::npos ? shortcut.substr(pos + 1) : shortcut;
     if (key.length() == 1 && key[0] >= L'0' && key[0] <= L'9')
     {
         out.vk = '0' + (key[0] - L'0');
@@ -45,4 +56,13 @@ bool ParseShortcut(const std::wstring& shortcut, HotkeyInfo& out)
     }
 
     return false;
+}
+
+bool ParseShortcut(const std::wstring& shortcut, HotkeyInfo& out)
+{
+    out.modifiers = 0;
+    out.vk = 0;
+    std::wstring upperCasedShortcut = getUpperCasedShortcut(shortcut);
+    ParseModifiers(upperCasedShortcut, out);
+    return ParseLetterAndNumbers(out, upperCasedShortcut);
 }
