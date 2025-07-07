4172dbc073dbc88816951f6c7bfb1cf13fa40d51
Dominik Cebula <dominikcebula@gmail.com>
2025-07-07 22:05:21 +0200

    Adding better error support for issues when chaning the display

--- src/Resolution.cpp
+++ src/Resolution.cpp
@@ -1,6 +1,30 @@
 #include "Resolution.h"
 
+#include <string>
 #include <Windows.h>
+
+static std::wstring getErrorMessage(LONG result)
+{
+    switch (result)
+    {
+    case DISP_CHANGE_BADDUALVIEW:
+        return L"The system is DualView capable. Settings change unsuccessful.";
+    case DISP_CHANGE_BADFLAGS:
+        return L"An invalid set of flags was passed in.";
+    case DISP_CHANGE_BADMODE:
+        return L"The graphics mode is not supported.";
+    case DISP_CHANGE_BADPARAM:
+        return L"An invalid parameter or flag was passed in.";
+    case DISP_CHANGE_FAILED:
+        return L"The display driver failed the specified graphics mode.";
+    case DISP_CHANGE_NOTUPDATED:
+        return L"Unable to write settings to the registry.";
+    case DISP_CHANGE_RESTART:
+        return L"The computer must be restarted for the graphics mode to work.";
+    default:
+        return L"Unknown error occurred while changing resolution.";
+    }
+}
 
 void SetResolution(int width, int height)
 {
@@ -12,6 +36,10 @@ void SetResolution(int width, int height)
     LONG result = ChangeDisplaySettings(&dm, CDS_UPDATEREGISTRY);
     if (result != DISP_CHANGE_SUCCESSFUL)
     {
-        MessageBox(nullptr, L"Failed to change resolution!", L"ResoSwitch - Error", MB_OK | MB_ICONERROR);
+        const std::wstring errorMsg = getErrorMessage(result);
+
+        MessageBox(nullptr, std::wstring(L"Failed to change resolution: ").append(errorMsg).c_str(),
+                   L"Error",
+                   MB_OK | MB_ICONERROR);
     }
 }
